aa4d0e243dad01a7ad394f1dbd5b57669b603c05
LuanaPorciuncula <lpb@cin.ufpe.br>
2020-10-16 18:41:14 -0300

    adicionada nova funcao em OperacoesComNat para responder a letra b da questao da lista correspondente

--- HaskellAvaliacao-Lista2/OperacoesComNat.hs
+++ HaskellAvaliacao-Lista2/OperacoesComNat.hs
@@ -6,3 +6,8 @@ data Nat = Zero | Succ Nat deriving (Eq,Show,Read)
 int2Nat :: Int -> Nat
 int2Nat 0 = Zero
 int2Nat n = Succ (int2Nat (n-1))
+
+-- b)
+nat2Int :: Nat -> Int
+nat2Int Zero = 0
+nat2Int (Succ n) = 1 + nat2Int n
