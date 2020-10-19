eb743195a56c1763ec31eb6e31b73bb793ee2467
LuanaPorciuncula <lpb@cin.ufpe.br>
2020-10-18 21:52:17 -0300

    adicionada letra b da lista, que ordena uma lista de inteiros fazendo uso de uma arvore de busca

--- HaskellAvaliacao-Lista2/ArvoreDeBusca.hs
+++ HaskellAvaliacao-Lista2/ArvoreDeBusca.hs
@@ -10,5 +10,13 @@ buildSearchTree list
  | null (tail list) = Node (head list) Empty Empty
  | otherwise        = Node (head list) (buildSearchTree [x | x <- (tail list), x < (head list)]) (buildSearchTree [x | x <- (tail list), x >= (head list)])
 
+-- b)
+searchTreeSort :: [Int] -> [Int]
+searchTreeSort list = (treeSort . buildSearchTree) list
+
+treeSort :: Tree Int -> [Int]
+treeSort Empty = []
+treeSort (Node n Empty Empty) = [n]
+treeSort (Node n tree1 tree2) = treeSort tree1 ++ [n] ++ treeSort tree2
 
  
