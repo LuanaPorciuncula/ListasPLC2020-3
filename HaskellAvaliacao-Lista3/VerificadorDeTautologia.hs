80995965089f6bd64529cff25638cc62dd543319
LuanaPorciuncula <lpb@cin.ufpe.br>
2020-11-05 10:16:23 -0300

    adicionadas funcoes para gerar todas as valoracoes possiveis para um dado numero de variaveis e uma funcao que converte Inteiros->numero em binario, necessarias para resolver a letra e

--- HaskellAvaliacao-Lista3/VerificadorDeTautologia.hs
+++ HaskellAvaliacao-Lista3/VerificadorDeTautologia.hs
@@ -30,3 +30,18 @@ vars (And prop1 prop2)     = vars prop1 ++ vars prop2
 vars (Or prop1 prop2)      = vars prop1 ++ vars prop2
 vars (Implies prop1 prop2) = vars prop1 ++ vars prop2
 vars (Iff prop1 prop2)     = vars prop1 ++ vars prop2
+
+--e)
+bools :: Int -> [[Bool]]
+bools n = bools' (2^n -1) n
+
+bools' :: Int -> Int -> [[Bool]]
+bools' 0 n = [(toBinary 0 n)]
+bools' i n = (toBinary i n):(bools' (i-1) n)
+
+toBinary :: Int -> Int -> [Bool]
+toBinary n bsize 
+ | (bsize == 1) && (mod n 2 == 1) = [True]
+ | (bsize == 1) && (mod n 2 == 0) = [False]
+ | mod n 2 == 1                   = True:(toBinary (div n 2) (bsize - 1))
+ | otherwise                      = False:(toBinary (div n 2) (bsize - 1))
