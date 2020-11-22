1647c58d10cc16eb6b179b5db674abcb8e6bd5a3
LuanaPorciuncula <lpb@cin.ufpe.br>
2020-11-22 20:48:39 -0300

    corrigida forma que os ids dos clientes eram atribuidos

--- haskell_java_avaliacao_lista4/MaquinaDeRefri.hs
+++ haskell_java_avaliacao_lista4/MaquinaDeRefri.hs
@@ -5,19 +5,22 @@ import Control.Concurrent.MVar
 --pCola gPoloNorte gQuate
 --P-Cola, Guaraná Polo Norte e Guaraná Quate.
 
-drink :: Int -> Int -> String -> MVar Int -> MVar Bool -> IO ()
+drink :: MVar Int -> Int -> String -> MVar Int -> MVar Bool -> IO ()
 drink idCliente qtdCliente idRefri refri disp
   = do usando <- takeMVar disp
-       newRefri <- (takeMVar refri)
-       -- 1000ms == 1000000 microseconds
-       threadDelay 1000000
-       --print(idRefri++" "++show (newRefri-300))
-       putMVar refri (newRefri - 300)
-       putMVar disp usando
-       print ("O cliente "++(show idCliente)++" do refrigerante "++idRefri++" esta enchendo seu copo")
-       if (idCliente+3 <= qtdCliente)
-         then do drink (idCliente+3) qtdCliente idRefri refri disp
-         else do print ("Acabaram os clientes de "++idRefri)
+       newCliente <- takeMVar idCliente
+       if (newCliente <= qtdCliente)
+         then do newRefri <- (takeMVar refri)
+                 -- 1000ms == 1000000 microseconds
+                 threadDelay 1000000
+                 --print(idRefri++" "++show (newRefri-300))
+                 putMVar refri (newRefri - 300)
+                 print ("O cliente "++(show newCliente)++" do refrigerante "++idRefri++" esta enchendo seu copo")
+                 putMVar disp usando
+                 putMVar idCliente (newCliente+1)
+                 drink idCliente qtdCliente idRefri refri disp
+         else do putMVar disp usando
+                 putMVar idCliente newCliente
 
 checkNRefill :: String -> MVar Int -> MVar Bool -> IO ()
 checkNRefill idRefri refri disp
@@ -39,18 +42,21 @@ main
   = do pCola      <- newMVar 2000
        gPoloNorte <- newMVar 2000
        gQuate     <- newMVar 2000
+       
+       idCliente <- newMVar 1
 
+       --Quantidade de clientes
        let qtdCliente = 30
 
        disp <- newMVar True
 
-       forkIO (drink 1 qtdCliente "pCola" pCola disp)
+       forkIO (drink idCliente qtdCliente "pCola" pCola disp)
        forkIO (checkNRefill "pCola" pCola disp)
        
-       forkIO (drink 2 qtdCliente "Guarana Polo Norte" gPoloNorte disp)
+       forkIO (drink idCliente qtdCliente "Guarana Polo Norte" gPoloNorte disp)
        forkIO (checkNRefill "Guarana Polo Norte" gPoloNorte disp)
 
-       forkIO (drink 3 qtdCliente "Guarana Quate" gQuate disp)
+       forkIO (drink idCliente qtdCliente "Guarana Quate" gQuate disp)
        forkIO (checkNRefill "Guarana Quate" gQuate disp)
        
        --Tempo em microsegundos que a maquina ira funcionar
